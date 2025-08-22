6a4d6ac7b12103bbf42008271762a05a390a6486
Jordan Marinov <jaisor@gmail.com>
2025-08-21 18:10:12 -0700

    build num increase only on release, and wifi power support for 8266

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
   #define BUILD_NUMBER "83"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.83 - 2025-08-21 18:01:19.958039"
+  #define VERSION "v0.1.83 - 2025-08-21 18:09:35.959600"
 #endif
 #ifndef VERSION_SHORT
   #define VERSION_SHORT "v0.1.83"
