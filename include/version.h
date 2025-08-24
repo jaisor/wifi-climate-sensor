b7c1aec66eab9670b03c72e4e0fbced94e19a5ac
Jordan Marinov <jaisor@gmail.com>
2025-08-24 10:15:43 -0700

    artifact downloading

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
   #define BUILD_NUMBER "83"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.83 - 2025-08-21 18:09:35.959600"
+  #define VERSION "v0.1.83 - 2025-08-24 10:03:17.138469"
 #endif
 #ifndef VERSION_SHORT
   #define VERSION_SHORT "v0.1.83"
