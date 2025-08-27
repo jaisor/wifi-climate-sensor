95fef8ca3f7b6da4351a1f0a6e53f976fac29513
Jordan Marinov <jaisor@gmail.com>
2025-08-27 13:31:36 -0700

    version bump

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "98"
+  #define BUILD_NUMBER "101"
 #endif
 #ifndef VERSION
-  #define VERSION "0.0.dev.98 - 2025-08-27 13:25:12.896551"
+  #define VERSION "0.0.dev.101 - 2025-08-27 13:31:26.309040"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "0.0.dev.98"
+  #define VERSION_SHORT "0.0.dev.101"
 #endif
